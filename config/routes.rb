2f720112ca7ce2f6c5456e26b96b3c0043108618
sugimoto519 <rqsnq531@gmail.com>
2025-01-27 05:59:50 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
-
+  devise_for :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
